b1e1be69adaae3ff63c51e4399ca511761f4a6c0
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-06 17:16:24 -0400

    fix: remove explicit created_at field, because t.timestamps will add this

--- db/migrate/20200406201615_create_rooms.rb
+++ db/migrate/20200406201615_create_rooms.rb
@@ -2,7 +2,6 @@ class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
       t.string :name
-      t.datetime :created_at
       t.references :created_by, references: :users, null: false, foreign_key: { to_table: :users }
       t.datetime :closed_at
 
