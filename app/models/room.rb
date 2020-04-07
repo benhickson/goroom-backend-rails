b4f1873ccf16a6f9aa7c39d9c574b247108ab2ed
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-06 20:15:34 -0400

    feat: add relationships to models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,3 @@
 class Room < ApplicationRecord
-  belongs_to :created_by
+  belongs_to :created_by, :class_name => 'User', :foreign_key => 'user_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 	has_secure_password
 
+	has_one :anonymous_user
+
+	has_many :rooms
 	has_many :memberships
 	has_many :groups, through: :memberships
 end
