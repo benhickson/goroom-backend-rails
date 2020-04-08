6ff688ec6af51380902da22add6d4998094acce7
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-07 23:18:01 -0400

    feat: add some boilerplate methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+	# make methods here available to other controllers:
+	# helper_method :logged_in?, :current_user
+
+	# run these methods before any controller action
+	# before_action :redirect_if_not_authorized
+	# before_action :set_current_user
+
+	private
+
+	def set_current_user
+		if session[:user_id]
+			@user = User.find(session[:user_id])
+		end
+	end
+
+	def redirect_if_not_authorized
+		redirect_to login_path unless logged_in?
+	end
+
+	# helper method for #redirect_if_not_authorized
+	def logged_in?
+		!!current_user
+	end
+
 end
