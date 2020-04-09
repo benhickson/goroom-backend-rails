bc58ff62154646cc9678f732c3faf4f6ccb95d90
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-09 17:38:41 -0400

    feat: add room name validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,11 @@
 class Room < ApplicationRecord
   belongs_to :creator, :class_name => 'User'
 
+  validates_uniqueness_of :name,
+    case_sensitive: false,
+    conditions: -> { where(closed_at: nil) },
+    message: '- That name isn\'t available.'
+
   def self.open
     self.where('closed_at IS NULL')
   end
