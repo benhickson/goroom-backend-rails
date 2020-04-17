c9f34d0d2ee6958a688e289ab79cf4ca18d3be9c
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-04-17 14:04:20 -0400

    temp feat: add email to show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 		user = User.find(params[:id])
 		render json: {
 			id: user.id, 
+			email: user.email,
 			display_name: user.display_name,
 			anon_display_name: user.anon_display_name
 		}, status: :ok
